84a89d4b02fa1eadd262b735531f8b995157f9ea
22722679 <116802544+22722679@users.noreply.github.com>
2023-08-21 10:48:15 +0800

    Add files via upload

---
+++ config/config.go
@@ -0,0 +1,12 @@
+package config
+
+//ftp服务器地址
+const FtpIp = "localhost:21"
+const FtpName = "a"
+const FtpPass = "duibuqi.123"
+const HeartTime = 60 * 60
+
+//mysql登录信息
+const User = "root"
+const PassWord = "duibuqi.123"
+const IpMessage = "106.54.210.139:3306"
