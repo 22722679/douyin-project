8f3ec184f17e993760d42282346a65f68745dc50
22722679 <116802544+22722679@users.noreply.github.com>
2023-08-21 14:23:17 +0800

    Update response.go

--- response.go
+++ response.go
@@ -1,36 +1,36 @@
-package main
+package包主
 
-import (
-	"ORM-simple/model"
-	"net/http"
+import进口 （
+	“ORM-简单/模型”"douyin-project/model"
+	“网络/http”"net/http"
 
-	"github.com/gin-gonic/gin"
-)
+	“github.com/gin-gonic/gin”"github.com/gin-gonic/gin"
+）
 
-type ResCode int64
+type类型 ResCode int64int64
 
-const CodeSuccess ResCode = 0
+constconst CodeSuccess ResCode = 00
 
-var codeMsgMap = map[ResCode]string{
-	CodeSuccess: "success",
+varvar codeMsgMap = 地图[ResCode]字符串{map[ResCode]string{
+	代码成功：“成功”，"success",
 }
 
-type ResponseData struct {
-	Code      ResCode     `json:"status_code"`
-	Msg       interface{} `json:"status_msg"`
-	VideoInfo interface{} `json:"video_list"`
+type类型响应数据结构{struct {
+	代码 ResCode `json:"status_code"``json:"status_code"`
+	消息接口{} `json:"status_msg"`interface{} `json:"status_msg"`
+	VideoInfo 接口{} `json:"video_list"`interface{} `json:"video_list"`
 }
 
-func (rc ResCode) Msg() string {
+funcfunc (rc ResCode) Msg() 字符串 {string {
 	msg, _ := codeMsgMap[rc]
 
-	return msg
+	返回消息return msg
 }
 
-func ResponseSuccess(c *gin.Context, videoList []*model.VideoInfo) {
-	c.JSON(http.StatusOK, &ResponseData{
-		Code:      CodeSuccess,
-		Msg:       CodeSuccess.Msg(),
-		VideoInfo: &videoList, //返回的应该为数据库中的信息，该信息videoList用mysql/feed中的SelectVideoInfo()获得
+funcfunc ResponseSuccess(c *gin.Context, videoList []*model.VideoInfo) {.Context, videoList []*model.VideoInfo) {
+	c.JSON(http.StatusOK, &ResponseData{.JSON(http.StatusOK, &ResponseData{
+		代码：代码成功，
+		消息：CodeSuccess.Msg(),.Msg(),
+		VideoInfo: &videoList, //返回的应该为数据库中的信息，该信息videoList用mysql/feed中的SelectVideoInfo()获得//返回的应该为数据库中的信息，该信息videoList用mysql/feed中的SelectVideoInfo()获得
 	})
 }
