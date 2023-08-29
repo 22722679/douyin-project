2927c6ce73837fe3e6965f8874460f5e5acf4cd8
22722679 <116802544+22722679@users.noreply.github.com>
2023-08-29 09:14:39 +0800

    Update router.go

--- router/router.go
+++ router/router.go
@@ -1,26 +1,51 @@
 package router
 
-import (
-	"douyin-project/mysql"
 
-	"douyin-project/controller"
+
+import (
+
+	"github.com/22722679/douyin-project/mysql"
+
+
+
+	"github.com/22722679/douyin-project/controller"
+
+
 
 	"github.com/gin-gonic/gin"
+
 	//"net/http"
+
 )
 
+
+
 func InitRouter(r *gin.Engine) {
+
 	r.GET("/douyin/feed", VideoInfoHandler)
+
 	r.POST("/douyin/user/register/", controller.Register)
+
 	r.POST("/douyin/user/login/", controller.Login)
+
 	r.GET("/douyin/user", controller.UserInfo)
+
 	r.POST("/douyin/publish/action/", controller.PublishAction)
+
 	r.GET("/douyin/publish/list/", controller.PublishList)
+
 	r.GET("/douyin/favorite/list/", controller.FavoriteList)
+
 }
 
+
+
 // 视频信息处理
+
 func VideoInfoHandler(c *gin.Context) {
+
 	data, _ := mysql.SelectVideoInfo()
+
 	ResponseSuccess(c, data)
+
 }
