70eaea4fe1e41041f8583bf17b5d4a08a9ebd8e0
22722679 <116802544+22722679@users.noreply.github.com>
2023-08-29 09:16:45 +0800

    Update user.go

--- mysql/user.go
+++ mysql/user.go
@@ -1,22 +1,42 @@
 package mysql
 
+
+
 //import "os/user"
 
+
+
 import (
+
 	"database/sql"
+
 	//"douyin-project/model"
+
 	"fmt"
+
 )
 
+
+
 func SelectUserName(userId int64) (string, error) {
+
 	var Name string
+
 	sqlStr := fmt.Sprintf("select name from user_infos where id = %d", userId)
+
 	if err := db.Select(&Name, sqlStr); err != nil {
+
 		if err == sql.ErrNoRows {
+
 			Name = "zxh1238"
+
 		}
+
 	}
+
 	//db.Select("name").Where("id = ?",userId).First(&Name)
+
 	return Name, nil
 
 }
+
