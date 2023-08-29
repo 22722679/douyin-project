be701e40a99b2252d56632058b4980fd538ea3b4
22722679 <116802544+22722679@users.noreply.github.com>
2023-08-29 09:18:06 +0800

    Update favorite.go

--- controller/favorite.go
+++ controller/favorite.go
@@ -1,23 +1,45 @@
 package controller
 
+
+
 import (
-	"douyin-project/service"
+
+	"github.com/22722679/douyin-project/service"
+
 	"net/http"
+
 	"strconv"
 
+
+
 	"github.com/gin-gonic/gin"
+
 )
 
+
+
 //视频列表查询
+
 func FavoriteList(ctx *gin.Context){
+
 	strUserId := ctx.Query("user_id")
+
 	userId, _ := strconv.Atoi(strUserId)
+
 	res, err := service.FavoriteList(uint(userId))
 
+
+
 	if err != nil {
+
 		ctx.JSON(http.StatusOK,res)
+
 		return 
+
 	}
+
 	ctx.JSON(http.StatusOK,res)
+
 	return
+
 }
