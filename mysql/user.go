f5ca57499b1260788d21d40588bbecedef092fd5
22722679 <116802544+22722679@users.noreply.github.com>
2023-08-30 15:07:13 +0800

    Update user.go

--- mysql/user.go
+++ mysql/user.go
@@ -1,30 +1,44 @@
 package mysql
 
-
-
 //import "os/user"
 
-
-
 import (
-
 	"database/sql"
 
 	//"douyin-project/model"
-
-	"fmt"
-
+  "strconv"
+  "fmt"
+	"github.com/22722679/douyin-project/model"
 )
 
+
+func CheckUserName(username string) uint{
+  // var registerInfo model.RegisterInfo
+  // 用户名  注册表？？？
+  var Id string
+  sqlStr := "SELECT id from users WHERE username = ?"
+  if err := db.Get(&Id,sqlStr, username);err != nil {
+    if err == sql.ErrNoRows {
+      return 0;
+    }
+    fmt.Printf("check ::%v",err);
+    return 0; 
+    
+  }
+  fmt.Printf("%v\n", Id);
+  intNum, _ := strconv.Atoi(Id)
+   return uint(intNum)
+  // return registerInfo
+}
 
 
 func SelectUserName(userId int64) (string, error) {
 
 	var Name string
 
-	sqlStr := fmt.Sprintf("select name from user_infos where id = %d", userId)
+	sqlStr := "select name from user_infos where id = ? "
 
-	if err := db.Select(&Name, sqlStr); err != nil {
+	if err := db.Select(&Name, sqlStr, userId); err != nil {
 
 		if err == sql.ErrNoRows {
 
@@ -34,9 +48,18 @@ func SelectUserName(userId int64) (string, error) {
 
 	}
 
-	//db.Select("name").Where("id = ?",userId).First(&Name)
 
 	return Name, nil
 
+}
+
+
+func InsertUserInfo(userInfo model.User) error{
+    sqlStr := "insert into users(username,password) values(?,?)"
+    if _,err := db.Exec(sqlStr,userInfo.Username, userInfo.Password); err != nil{
+      fmt.Printf("%v\n",err)
+      return err
+    }
+  return nil
 }
 
