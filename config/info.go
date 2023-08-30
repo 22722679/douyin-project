1cd9ea7bdf6df9828ee1882cbc593dac0ec0a7e5
22722679 <116802544+22722679@users.noreply.github.com>
2023-08-30 15:01:54 +0800

    Update info.go

--- config/info.go
+++ config/info.go
@@ -1,16 +1,31 @@
 package config
 
+
+
 //Video
+
 const CoverUrl = "https://static.www.tencent.com/uploads/2021/06/03/dca7d5f3043ce88886539c95f0fe0752.jpg"
+
 const PlayUrl = "https://static.www.tencent.com/uploads/2021/09/07/bd0a889911f5564edccba61e0a4842d8.mp4"
+
 const Tiele = "tradition Craft"
+
 const Id = 2
+
 const FavoriteCount = 3
+
 const CommentCount = 2
+
 //Userinfo
+
 const Avatar = "https://static.www.tencent.com/uploads/2021/06/03/dca7d5f3043ce88886539c95f0fe0752.jpg"
+
 const BackgroundImages = "https://www.tsinghua.edu.cn/__local/0/E9/B5/123588172AED93BD0193C0D83FA_DED4F3EB_901C6.jpg"
+
 const Signature = "感谢大家支持"
+
 const TotalFavorited = "320W"
+
 const WorkCount = 231
+
 const FavoriteCountl = 5214
