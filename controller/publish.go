9097590a1d9226d0d8d33b9f9ba57e1a7286311c
22722679 <116802544+22722679@users.noreply.github.com>
2023-08-30 15:01:01 +0800

    Update publish.go

--- controller/publish.go
+++ controller/publish.go
@@ -1,33 +1,108 @@
 package controller
 
+
+
 import (
 
-	//"database/sql"
-
 	"github.com/22722679/douyin-project/model"
+
 	"github.com/22722679/douyin-project/service"
 
-
+	"fmt"
 
 	"net/http"
 
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 
-
 )
 
-func Register(ctx *gin.Context){
-  request := new(model.ReAndLogRequest)
-  request.Username,_ = ctx.GetQuery("username");
-  request.Password,_ = ctx.GetQuery("password");
-  // err := ctx.ShouldBindQuery(request)
-  // fmt.Printf("%v\n", request); 
-  // if err != nil {
-  //   fmt.Println("解析失败")
-  //   return
-  // }
-  response := service.Register(request)
-  ctx.JSON(http.StatusOK,response)
-  return
+
+
+func PublishList(ctx *gin.Context) {
+
+	//解析
+
+	token := ctx.Query("token")
+
+	userIdStr := ctx.Query("user_id")
+
+
+
+	//获取当前状态登录用户的id、token
+
+	loginuser, _ := strconv.Atoi(token)
+
+	userId, _ := strconv.Atoi(userIdStr)
+
+
+
+	if loginuser == 0 {
+
+		response, err := service.PublishList(uint(userId), uint(userId))
+
+		if err != nil {
+
+			ctx.JSON(http.StatusInternalServerError, response)
+
+			return
+
+		}
+
+		ctx.JSON(http.StatusOK, response)
+
+	} else {
+
+		response, err := service.PublishList(uint(userId), uint(loginuser))
+
+		if err != nil {
+
+			ctx.JSON(http.StatusInternalServerError, response)
+
+			return
+
+		}
+
+		ctx.JSON(http.StatusOK, response)
+
+	}
+
+	return
+
 }
+
+
+
+func PublishAction(ctx *gin.Context) {
+
+	//参数解析
+
+	request := new(model.PublishActionRequest)
+
+	if err := ctx.ShouldBind(request); err != nil {
+
+		fmt.Println("解析失败")
+
+		return
+
+	}
+
+
+
+	userId, _ := ctx.Get("userId")
+
+	code_a, _ := service.PublishAction(request, userId.(uint))
+
+	ctx.JSON(http.StatusOK, &model.Response{
+
+		StatusCode: code_a,
+
+		StatusMsg:  model.GetMsg(code_a),
+
+	})
+
+	return
+
+}
+
