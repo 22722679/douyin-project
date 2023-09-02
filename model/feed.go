d33a669a0edd0c5621b8b6e57b0248f351d7437b
22722679 <116802544+22722679@users.noreply.github.com>
2023-09-02 16:01:05 +0800

    Update feed.go

--- model/feed.go
+++ model/feed.go
@@ -1,26 +1,52 @@
 package model
 
+
+
 import "gorm.io/gorm"
 
+
+
 //视频流信息
+
 type VideoInfo struct {
+
 	gorm.Model
+
 	Id            int      `json:"id" db:"author_id"`
+
 	PlayUrl       string   `json:"play_url" db:"play_url"`
+
 	CoverUrl      string   `json:"cover_url" db:"cover_url"`
+
 	FavoriteCount int      `json:"favorite_count" db:"favorite_count"`
+
 	CommentCount  int      `json:"comment_count" db:"comment_count"`
+
 	Title         string   `json:"title" db:"title"`
+
+	Author        UserInfo `json:"author"`      //视频作者信息
 }
 
+
+
 type Video struct {
+
 		gorm.Model
+
 	Id            int      `json:"id" db:"author_id"`
+
 	PlayUrl       string   `json:"play_url" db:"play_url"`
+
 	CoverUrl      string   `json:"cover_url" db:"cover_url"`
+
 	FavoriteCount int      `json:"favorite_count" db:"favorite_count"`
+
 	CommentCount  int      `json:"comment_count" db:"comment_count"`
+
 	Title         string   `json:"title" db:"title"`
+
 	Author        UserInfo `json:"author"`      //视频作者信息
+
 	IsFavorite    bool     `json:"is_favorite"` //是否点赞
+
 }
