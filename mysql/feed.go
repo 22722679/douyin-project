ca2acd244283d3016291b9703e632f3e0f718d3e
22722679 <116802544+22722679@users.noreply.github.com>
2023-09-02 16:05:50 +0800

    Update feed.go

--- mysql/feed.go
+++ mysql/feed.go
@@ -18,18 +18,6 @@ import (
 
 func SelectVideoInfo() (videoList []*model.VideoInfo, err error) {
 
-	//sqlStr := "select author_id, play_url, cover_url, favorite_count, comment_count, title from video_infos"
-
-
-
-	//if err := db.Select(&videoList, sqlStr).Table("video_infos"); err == nil {
-
-	//	fmt.Printf("数据库中没有视频")
-
-	//return nil, err
-
-	//}
-
 	sqlStr := "select author_id, play_url, cover_url, favorite_count, comment_count, title from video_infos"
 
 	if err := db.Select(&videoList, sqlStr); err != nil {
@@ -49,14 +37,6 @@ func SelectVideoInfo() (videoList []*model.VideoInfo, err error) {
 
 
 func SelectVideoInfoListByUserId(id int64) ([]*model.Video, error) {
-
-	//var videoList []*model.VideoInfo
-
-	//if err := db.Where("author_id = ?").Find(&videoList).Error; err != nil {
-
-	//	return nil, err
-
-	//}
 
 	var UserId []*model.Video
 
