475eb7fc1834606ea1aa61159001f7494021147e
22722679 <116802544+22722679@users.noreply.github.com>
2023-09-02 16:13:13 +0800

    Update publish.go

--- controller/publish.go
+++ controller/publish.go
@@ -69,40 +69,6 @@ func PublishList(ctx *gin.Context) {
 
 }
 
-
-
-func PublishAction(ctx *gin.Context) {
-
-	//参数解析
-
-	request := new(model.PublishActionRequest)
-
-	if err := ctx.ShouldBind(request); err != nil {
-
-		fmt.Println("解析失败")
-
-		return
-
-	}
-
-
-
-	userId, _ := ctx.Get("userId")
-
-	code_a, _ := service.PublishAction(request, userId.(uint))
-
-	ctx.JSON(http.StatusOK, &model.Response{
-
-		StatusCode: code_a,
-
-		StatusMsg:  model.GetMsg(code_a),
-
-	})
-
-	return
-
-}
-
 func Publish(ctx *gin.Context){
 	//获取userId
 	getUserId, _ := ctx.Get("user_id")
